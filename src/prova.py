5ef77dbd433f1cf74b0b182d438501317df1a867
Sabato Nocera <52103278+sabato-nocera@users.noreply.github.com>
2025-05-19 16:28:37 +0200

    Create prova.py

---
+++ src/prova.py
@@ -0,0 +1,7 @@
+import xml.sax
+
+parser = xml.sax.make_parser()
+myHandler = MyHandler()
+parser.setContentHandler(myHandler)
+parser.setFeature(feature_external_ges, True) # Noncompliant
+parser.parse('xxe.xml')
